7d86fab43892746fe92a616688c6090e564c47d2
Joel-Ralaison <hery.ralaison@gmail.com>
2024-05-17 12:11:09 +0300

    release

--- components/customComp/BodyBackground.tsx
+++ components/customComp/BodyBackground.tsx
@@ -1,5 +1,11 @@
-export default function BodyBackground() {
+import { memo } from "react";
+
+const BodyBackground = memo(function BodyBackground() {
   return (
-    <div className="absolute inset-0 -z-[1] h-full w-full bg-custom-light bg-contain dark:bg-custom-dark" />
+    <>
+      <div className="absolute inset-0 -z-[1] h-full w-full bg-custom-light bg-contain dark:bg-custom-dark" />
+    </>
   );
-}
+});
+
+export default BodyBackground;
