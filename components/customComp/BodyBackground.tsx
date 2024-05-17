785b314aa46b23b408373db832a9eb11c222a600
Joel-Ralaison <hery.ralaison@gmail.com>
2024-05-17 11:43:46 +0300

    fix: bg rendering

--- components/customComp/BodyBackground.tsx
+++ components/customComp/BodyBackground.tsx
@@ -1,13 +1,5 @@
-import { Suspense } from "react";
-
 export default function BodyBackground() {
   return (
-    <Suspense
-      fallback={
-        <div className="absolute inset-0 -z-[1] h-full w-full bg-background" />
-      }
-    >
-      <div className="absolute inset-0 -z-[1] h-full w-full bg-custom-light bg-contain dark:bg-custom-dark" />
-    </Suspense>
+    <div className="absolute inset-0 -z-[1] h-full w-full bg-custom-light bg-contain dark:bg-custom-dark" />
   );
 }
